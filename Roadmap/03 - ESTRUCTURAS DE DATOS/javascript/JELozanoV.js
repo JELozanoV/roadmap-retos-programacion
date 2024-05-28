39d85e3b7ce8499adb094404f9da9a5e96413a08
JELozanoV <johnlozano331@outlook.com>
2024-05-27 22:50:06 -0500

    Cambios en algunas lineas

--- Roadmap/03 - ESTRUCTURAS DE DATOS/javascript/JELozanoV.js
+++ Roadmap/03 - ESTRUCTURAS DE DATOS/javascript/JELozanoV.js
@@ -33,7 +33,78 @@ let arrNumeros = [
 let numerosDispersos = []
 numerosDispersos[10] = 30 // Los 10 indices anteriores a el 10 estaran vacios 
 
-console.log(numerosDispersos);
+// Operadores de insercion en arrays 
 
+numeros.push(7,8) // Añade uno o mas elementos al final del array
+numeros.unshift(-2,-1,0) // Añade uno o mas elementos al inicio del arreglo
 
+// Splice
+
+numeros.splice(2, 0, 3, 4); // Sintaxis array.splice(indice, numeroDeElementosAEliminar, elemento1, ..., elementoN)
+
+// Spread operator 
+
+let array1 = [1, 2, 3];
+let array2 = [4, 5, 6];
+
+let combinado = [...array1, ...array2]; // Spread operator descompone el array y combina los arrays
+
+//Borrado 
+
+// Pop 
+
+let numeros2 = [1, 2, 3, 4, 5];
+let eliminado = numeros.pop(); // Elimina el último elemento de un array y lo devuelve
+console.log(numeros); // [1, 2, 3, 4]
+console.log(eliminado); // 5
+
+// shift
+
+let numeros3 = [1, 2, 3, 4, 5];
+let eliminado2 = numeros.shift(); // Elimina el primer elemento de un array y lo devuelve
+console.log(numeros); // [2, 3, 4, 5]
+console.log(eliminado); // 1
+
+// delete Operator
+
+let numeros4 = [1, 2, 3, 4, 5];
+delete numeros[2]; // Elimina el elemento en el índice 2
+console.log(numeros); // [1, 2, undefined, 4, 5]
+
+// slice 
+
+let frutas2 = ['manzana', 'banana', 'cereza', 'durazno', 'kiwi'];
+
+// Usar slice() para obtener una porción del array
+let algunasFrutas = frutas.slice(1, 4);
+
+console.log(algunasFrutas); // ['banana', 'cereza', 'durazno']
+console.log(frutas); // ['manzana', 'banana', 'cereza', 'durazno', 'kiwi']
+
+//Actualizacion
+
+frutas2[3] = "fruta actualizada"
+
+//Ordenar 
+
+frutas2.sort()
+console.log(frutas2);
+// Objetos 
+
+let persona = {
+    nombre:"Juan",
+    edad: 30
+}
+
+// Crear un set
+let conjunto = new Set([1, 2, 3, 4, 5]);
+console.log(conjunto);
+// Agregar un valor al set
+conjunto.add(6);
+
+// Verificar si un valor está en el set
+console.log(conjunto.has(3)); // true
+
+// Eliminar un valor del set
+conjunto.delete(4);
 
